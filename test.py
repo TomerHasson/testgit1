b5f6b303141f4926fef6393d1677f540fcaeb2ef
TomerHasson <52401321+TomerHasson@users.noreply.github.com>
2020-01-20 22:29:51 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import time
+time.sleep(10)
+i=0
+while True:
+    fname="demofile{}.txt".format(i)
+    print("creating file ", fname)
+    f = open(fname, "a")
+    f.write("Now the file has more content!")
+    f.close()
+    i+=1
+    time.sleep(60*2)
